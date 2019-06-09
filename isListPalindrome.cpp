acbf6071fe6b7cb14db63eb1099a1a4d2b4f06b6
Rajdeep Mallick <41420543+rajdeepmallick@users.noreply.github.com>
2019-06-09 23:43:07 +0530

    Check if Linked List is Palindrome
    https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1

---
+++ isListPalindrome.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+/*This is a function problem.You only need to complete the function given below*/
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+bool isPalindrome(Node *head)
+{
+    stack<int>s;
+    Node *p=head;
+    while(p!=NULL){
+        s.push(p->data);
+        p=p->next;
+    }
+    p=head;
+    while(p!=NULL){
+        if(p->data!=s.top())
+            return false;
+        p=p->next;
+        s.pop();
+    }
+    return true;
+}
