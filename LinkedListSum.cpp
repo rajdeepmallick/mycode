bce6769f67452d03a9b165786d4e3efafc80339f
Rajdeep Mallick <41420543+rajdeepmallick@users.noreply.github.com>
2019-06-10 21:10:46 +0530

    Find the Sum of Last N nodes of the Linked List
    https://practice.geeksforgeeks.org/problems/find-the-sum-of-last-n-nodes-of-the-linked-list/1

---
+++ LinkedListSum.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node {
+	int data;
+	struct Node* next;
+};
+void append(struct Node** headRef, int newData)
+{
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+	struct Node *last = *headRef;
+	new_node->data = newData;
+	new_node->next = NULL;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return;
+}
+int sumOfLastN_Nodes(struct Node* head, int n);
+// Driver program to test above
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        struct Node* head = NULL;
+        int n,m,tmp;
+        cin>>m>>n;
+        while(m--){
+            cin>>tmp;
+            append(&head, tmp);
+        }
+        cout<<sumOfLastN_Nodes(head, n)<<endl;
+    }
+	return 0;
+}
+
+/*This is a function problem.You only need to complete the function given below*/
+/*Structure of the node of the linled list is as
+struct Node {
+	int data;
+	struct Node* next;
+};*/
+// your task is to complete this function 
+// function should return sum of last n nodes
+Node* rev(Node *node){
+    Node *prev=NULL,*ptr=node,*Next;
+    while(ptr!=NULL){
+        Next=ptr->next;
+        ptr->next=prev;
+        prev=ptr;
+        ptr=Next;
+    }
+    return prev;
+}
+
+int sumOfLastN_Nodes(struct Node* head, int n)
+{
+    int sum=0;
+    head=rev(head);
+    while(n--){
+        sum=sum+head->data;
+        head=head->next;
+    }
+    return sum;
+}
